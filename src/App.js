5ba6d07625837004338e9b9d396a415c3a4e0f2b
ThePainter <59207693+Saidfatah@users.noreply.github.com>
2022-06-04 13:01:45 +0100

    removed Konva integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // Dependencies
 import React, { useRef } from "react";
 import Grid from "./Grid";
-import { Layer, Stage, Star, Text } from "react-konva";
 // Styles
 import "./tailwind.output.css";
 
@@ -55,35 +54,6 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-500">
       <Grid {...{ HEIGHT, WIDTH }} />
-      <Stage width={WIDTH} height={HEIGHT}>
-        <Layer>
-          <Text text="Try to drag a star" />
-          {stars.map((star) => (
-            <Star
-              key={star.id}
-              id={star.id}
-              x={star.x}
-              y={star.y}
-              numPoints={5}
-              innerRadius={20}
-              outerRadius={40}
-              fill="#89b717"
-              opacity={0.8}
-              draggable
-              rotation={star.rotation}
-              shadowColor="black"
-              shadowBlur={10}
-              shadowOpacity={0.6}
-              shadowOffsetX={star.isDragging ? 10 : 5}
-              shadowOffsetY={star.isDragging ? 10 : 5}
-              scaleX={star.isDragging ? 1.2 : 1}
-              scaleY={star.isDragging ? 1.2 : 1}
-              onDragStart={handleDragStart}
-              onDragEnd={handleDragEnd}
-            />
-          ))}
-        </Layer>
-      </Stage>
       {/* <canvas
         ref={dragDropCanvasRef}
         id="canvas2"
